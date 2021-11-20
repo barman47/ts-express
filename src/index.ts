809f2d98c49d97ac5e6106a8beecdb96a0abcbad
barman47 <nomsouzoanya@yahoo.co.uk>
2021-11-20 11:18:18 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cookieSession from 'cookie-session';
+
+import './controllers/LoginController';
+import './controllers/RootController';
+
+import { AppRouter } from './AppRouter'
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['fskfskkfsnfsf'] }));
+app.use(AppRouter.getInstance());
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
